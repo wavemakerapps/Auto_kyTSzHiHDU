270f71f364b04e75d5e4603aa75d8d4c823a5f0b
root <root@61949b3d2f95>
2017-07-18 07:22:05 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_kyTSzHiHDU",
+  "homePage" : "Main",
+  "name" : "Auto_kyTSzHiHDU",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
